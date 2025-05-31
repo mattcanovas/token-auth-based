7620c9c6501314c796d53c21d2f8382f1ef2ce24
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-30 23:52:29 -0300

    Implementing UserDetails

---
+++ src/main/java/com/github/mattcanovas/token_auth_based/security/user/details/UserDetailsImpl.java
@@ -0,0 +1,38 @@
+package com.github.mattcanovas.token_auth_based.security.user.details;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.github.mattcanovas.token_auth_based.entities.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+        .stream()
+        .map(role -> new SimpleGrantedAuthority(role.getUserRole().name()))
+        .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+    
+}
---
+++ src/main/java/com/github/mattcanovas/token_auth_based/security/user/details/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.github.mattcanovas.token_auth_based.security.user.details;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.github.mattcanovas.token_auth_based.entities.User;
+import com.github.mattcanovas.token_auth_based.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        super();
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        return new UserDetailsImpl(user);
+    }
+
+}
