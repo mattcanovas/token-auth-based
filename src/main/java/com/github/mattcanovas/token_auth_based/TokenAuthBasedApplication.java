94ca85e9ad4a01416e79419145130078d84c2d59
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-30 23:10:47 -0300

    Creating project

---
+++ src/main/java/com/github/mattcanovas/token_auth_based/TokenAuthBasedApplication.java
@@ -0,0 +1,13 @@
+package com.github.mattcanovas.token_auth_based;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mattcanovas/token_auth_based/TokenAuthBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mattcanovas.token_auth_based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenAuthBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
